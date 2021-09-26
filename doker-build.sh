d953f60d63ac5324e23f607e96e8e042940f77a7
Amit Ghosh <amit.ghosh@cefalo.com>
2021-09-26 22:41:10 +0600

    add docker and docker compose

---
+++ doker-build.sh
@@ -0,0 +1,6 @@
+docker build -t mse-client client/
+docker build -t mse-mse-event-bus event-bus/
+docker build -t mse-post posts/
+docker build -t mse-comment comments/
+docker build -t mse-moderation moderation/
+docker build -t mmse-query query/
